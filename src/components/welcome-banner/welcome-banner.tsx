59d81d3c45c5e1b5c2d9340f5ac085064b586711
Artem Koziar <koziar.artem@gmail.com>
2022-09-15 13:21:34 +0300

    Add Welcome title

--- src/components/welcome-banner/welcome-banner.tsx
+++ src/components/welcome-banner/welcome-banner.tsx
@@ -1,9 +1,14 @@
 import { FC } from 'react';
 
+import { ConnectButton } from '@shared/components';
+
+import styles from './welcome-banner.module.scss';
+
 export const WelcomeBanner: FC = () => {
   return (
     <div>
-      <h2>Welcome</h2>
+      <h2 className={styles.title}>Welcome</h2>
+      <ConnectButton />
     </div>
   );
 };
