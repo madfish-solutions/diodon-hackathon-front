80af76b06b1cc0131e059d136fe78f1885d5ed21
Inokentii Mazhara <keshan3262@gmail.com>
2022-09-23 18:21:06 +0300

    Implement closing a position

--- src/components/markets-list/use-markets.vm.ts
+++ src/components/markets-list/use-markets.vm.ts
@@ -1,12 +1,15 @@
 import { useEffect } from 'react';
 
+import { ClearingHouse } from '@blockchain/facades';
+import { AMMS, CLEARING_HOUSE_ADDRESS } from '@config/environment';
 import { useApi, useAuthStore, usePositionsStore } from '@shared/hooks';
 import { useMarketsStore } from '@shared/hooks/use-markets-store';
+import { MarketId } from '@shared/types';
 
 export const useMarketsViewModel = () => {
   const api = useApi();
 
-  const { address } = useAuthStore();
+  const { address, connection } = useAuthStore();
 
   const marketsStore = useMarketsStore();
   const { markets } = marketsStore;
@@ -31,6 +34,17 @@ export const useMarketsViewModel = () => {
       }
       await api.call(async () => {
         await positionsStore.loadPositions(address);
+        if (connection) {
+          const clearingHouse = new ClearingHouse(connection.provider, CLEARING_HOUSE_ADDRESS, connection.signer);
+          // eslint-disable-next-line no-console
+          console.log(
+            await Promise.all(
+              [MarketId.AMD, MarketId.AAPL].map(async marketId =>
+                clearingHouse.getPosition(AMMS[marketId], address).catch(e => e)
+              )
+            )
+          );
+        }
       });
     })();
     // eslint-disable-next-line
