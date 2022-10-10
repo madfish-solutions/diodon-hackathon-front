e96043963111904cb6b226eadbdd296d89450e93
Artem Koziar <koziar.artem@gmail.com>
2022-10-10 13:04:12 +0300

    remove interfaces

--- src/perpetual-contracts-interface/facades/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './amm';
-export * from './bridge';
-export * from './chainlink';
-export * from './clearingHouse';
-export * from './clearingHouseViewer';
-export * from './common';
-export * from './erc20';
