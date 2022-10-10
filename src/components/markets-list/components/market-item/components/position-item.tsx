226fcc0f85372aab5e67cf543e2b2e342d3be7cb
Artem Koziar <koziar.artem@gmail.com>
2022-10-10 13:28:40 +0300

    Refactor MarketItem

---
+++ src/components/markets-list/components/market-item/components/position-item.tsx
@@ -0,0 +1,50 @@
+import { FC } from 'react';
+
+import { Position } from '@api/positions';
+import { Button, PositionTypeIcon } from '@shared/components';
+import { GetUsdView, PercentView, TokensView } from '@shared/helpers';
+
+import { Cell } from '../../../../cell';
+import styles from '../market-item.module.scss';
+
+interface Props {
+  position: Position;
+  onClose: () => void;
+  positionBeingClosed: boolean;
+}
+
+export const PositionItem: FC<Props> = ({ position, positionBeingClosed, onClose }) => {
+  return (
+    <div className={styles.position}>
+      <div className={styles.positionTypeWrapper}>
+        <PositionTypeIcon type={position.type} width={64} height={64} style={{ marginRight: 8 }} />
+      </div>
+      <div className={styles.detailsPosition}>
+        <Cell label="Position Amount">
+          <TokensView suffix={position.marketId} amount={position.amountTokens} dollarEquivalent={position.amountUsd} />
+        </Cell>
+        <Cell label="Profit / Loss">
+          <PercentView amount={position.pnlPercent} pnl />
+        </Cell>
+        <Cell label="Open Price">
+          <GetUsdView amount={position.avgOpenPriceUsd} />
+        </Cell>
+        <Cell label="Margin Level">
+          <PercentView amount={12} />
+        </Cell>
+      </div>
+      <div className={styles.lastElementWrapper}>
+        <Button
+          onClick={event => {
+            event.stopPropagation();
+            onClose();
+          }}
+          className={styles.manageButton}
+          disabled={positionBeingClosed}
+        >
+          Close
+        </Button>
+      </div>
+    </div>
+  );
+};
--- src/components/markets-list/components/market-item/market-item.tsx
+++ src/components/markets-list/components/market-item/market-item.tsx
@@ -3,14 +3,10 @@ import { FC, useState } from 'react';
 import { observer } from 'mobx-react-lite';
 
 import { MarketData } from '@api/markets';
-import { MarginSlider } from '@components/account/components/margin-slider';
-import { Cell } from '@components/cell';
-import { BarChart } from '@shared/charts/bar-chart';
-import { Button } from '@shared/components';
-import { MarketIcon } from '@shared/components/market-icon';
-import { PositionTypeIcon } from '@shared/components/position-type-icon';
-import { getMultiplierView, GetUsdView, PercentView, TokensView } from '@shared/helpers';
 
+import { MarketInfo } from './components/market-info';
+import { PositionItem } from './components/position-item';
+import { PositionItemFull } from './components/position-item-full';
 import styles from './market-item.module.scss';
 import { useMarketItemViewModel } from './use-market-item.vm';
 
@@ -34,134 +30,25 @@ export const MarketItem: FC<Props> = observer(({ market }) => {
   return (
     <div className={styles.itemButton} onClick={() => seFullView(prev => !prev)}>
       <div className={styles.item}>
-        <div className={styles.market}>
-          <div style={{ textAlign: 'start', minWidth: '100px' }}>
-            <div style={{ textAlign: 'center', width: 50 }}>
-              <div>
-                <MarketIcon className={styles.marketItem} marketId={market.marketId} width={48} height={48} />
-              </div>
-              <div>
-                <b>{market.marketId}</b>
-              </div>
-            </div>
-          </div>
-          <div className={styles.details}>
-            <Cell label="Market Price" className={styles.Cell}>
-              <GetUsdView amount={market.marketPriceUsd} percentEquivalent={marketPriceChangePercentage} />
-            </Cell>
-            <Cell label="Index Price" className={styles.Cell}>
-              <GetUsdView amount={market.indexPriceUsd} percentEquivalent={indexPriceChangePercentage} />
-            </Cell>
-            <Cell label="Funding rate 8h" className={styles.Cell}>
-              <PercentView amount={market.fundingRateChange8Percent} decimalPlaces={4} />
-            </Cell>
-            <Cell label="Volume 24h" className={styles.Cell}>
-              <TokensView prefix="$" amount={market.volume24Usd} />
-            </Cell>
-          </div>
-          {!position ? (
-            <div className={styles.lastElementWrapper}>
-              <Button
-                onClick={event => {
-                  event.stopPropagation();
-                  openHandler();
-                }}
-                className={styles.openButton}
-                disabled={!isConnected}
-              >
-                Open
-              </Button>
-            </div>
+        <MarketInfo
+          market={market}
+          marketPriceChangePercentage={marketPriceChangePercentage}
+          indexPriceChangePercentage={indexPriceChangePercentage}
+          isConnected={isConnected}
+          openHandler={openHandler}
+          position={position}
+        />
+        {position &&
+          (fullView ? (
+            <PositionItemFull
+              position={position}
+              chartData={chartData}
+              onClose={closeHandler}
+              positionBeingClosed={positionBeingClosed}
+            />
           ) : (
-            <div className={styles.lastElementWrapper} />
-          )}
-        </div>
-        {fullView && position && (
-          <div className={styles.positionFull}>
-            <div className={styles.sidePanel}>
-              <div className={styles.detailsPositionFull}>
-                <Cell label="Position Amount">
-                  <TokensView amount={position.amountTokens} dollarEquivalent={position.amountUsd} />
-                </Cell>
-                <Cell label="Profit / Loss">
-                  <PercentView amount={position.pnlPercent} pnl />
-                </Cell>
-                <Cell label="Open Price">
-                  <GetUsdView amount={position.avgOpenPriceUsd} />
-                </Cell>
-                <Cell label="Margin Level">
-                  <PercentView amount={13} />
-                </Cell>
-                <Cell label="Leverage">{getMultiplierView(position.margin)}</Cell>
-              </div>
-              <PositionTypeIcon type={position.type} width={64} height={64} style={{ marginRight: 8 }} />
-            </div>
-            <div className={styles.mainPanel}>
-              <div className={styles.headerInfo}>
-                <span className={styles.mainText}>Market price, USD</span>
-                <span className={styles.secondaryText}>Index price, USD</span>
-                <span className={styles.secondaryText}>Funding rate, %</span>
-              </div>
-              <BarChart data={chartData} />
-              <div className={styles.footerInfo}>
-                <div>
-                  <div className={styles.marginLevel}>Margin level:</div>
-                  <div className={styles.explanation}>Low risk – you’re going to get a good night’s sleep.</div>
-                </div>
-                <MarginSlider value={14} className={styles.slider} />
-                <div className={styles.lastElementWrapper}>
-                  <Button
-                    onClick={event => {
-                      event.stopPropagation();
-                      closeHandler();
-                    }}
-                    className={styles.manageButton}
-                    disabled={positionBeingClosed}
-                  >
-                    Close
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
-        {position && !fullView ? (
-          <div className={styles.position}>
-            <div className={styles.positionTypeWrapper}>
-              <PositionTypeIcon type={position.type} width={64} height={64} style={{ marginRight: 8 }} />
-            </div>
-            <div className={styles.detailsPosition}>
-              <Cell label="Position Amount">
-                <TokensView
-                  suffix={position.marketId}
-                  amount={position.amountTokens}
-                  dollarEquivalent={position.amountUsd}
-                />
-              </Cell>
-              <Cell label="Profit / Loss">
-                <PercentView amount={position.pnlPercent} pnl />
-              </Cell>
-              <Cell label="Open Price">
-                <GetUsdView amount={position.avgOpenPriceUsd} />
-              </Cell>
-              <Cell label="Margin Level">
-                <PercentView amount={12} />
-              </Cell>
-            </div>
-            <div className={styles.lastElementWrapper}>
-              <Button
-                onClick={event => {
-                  event.stopPropagation();
-                  closeHandler();
-                }}
-                className={styles.manageButton}
-                disabled={positionBeingClosed}
-              >
-                Close
-              </Button>
-            </div>
-          </div>
-        ) : null}
+            <PositionItem position={position} onClose={closeHandler} positionBeingClosed={positionBeingClosed} />
+          ))}
       </div>
     </div>
   );
