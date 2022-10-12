9e3ce3ea45cda527ce9fa65a86d91211a7f847b2
Artem Koziar <koziar.artem@gmail.com>
2022-10-12 13:16:51 +0300

    Add page title

--- src/modules/markets/markets.page.tsx
+++ src/modules/markets/markets.page.tsx
@@ -5,6 +5,7 @@ import styles from './markets.page.module.scss';
 export const MarketsPage = () => {
   return (
     <div className={styles.root}>
+      <h1 className={styles.title}>Markets</h1>
       <MarketsList />
     </div>
   );
